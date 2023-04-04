4d390f73fa7f22393e1578a27150f1da387ee173
Alessandra-Casu <alessandracasu919@gmail.com>
2023-04-04 15:40:24 +0200

    add const

--- script.js
+++ script.js
@@ -4,3 +4,18 @@
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito23 */
+
+
+
+
+
+const yourName = parseInt(prompt('Inserisci il tuo nome: '));
+const lastName = parseInt(prompt('Inserisci il tuo cognome: '));
+const color = parseInt(prompt('Inserisci il tuo colore preferito: '));
+
+
+
+const password = '23';
+
+
+document.getElementById('result').innerHTML = password; 
